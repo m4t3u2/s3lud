1546d4bc1d39cffb1fc1f6f464b45c93e6dc2b06
Mateus Simon <ms.mateus@hotmail.com>
2022-07-07 22:16:26 -0300

    Structure.

---
+++ src/main/java/com/s3lud/AmazonS3LudApplication.java
@@ -0,0 +1,13 @@
+package com.s3lud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonS3LudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonS3LudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s3lud/AmazonS3LudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3lud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonS3LudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
